0cc22fc1d824260a364c49128637d11e34f0a960
Matichai <notnoth43@gmail.com>
2022-02-24 15:22:51 +0700

    .

--- Dialogflow/index.js
+++ Dialogflow/index.js
@@ -828,7 +828,7 @@ const myexp = ((request, response) => {
 
     const measureAlcohalInBlood = async () => {
 
-        //let gender = agent.parameters ;
+        let {gender} = agent.parameters ;
 
         agent.add(`คุณกำลังอยู่ในฟังก์ชั่นการวัดแอลกอฮอล์ในเลือด`);
         //agent.add(`ค่า Gender : ${gender} `) ;
