04b10c537e83ed6c92ae76d6e672cc17312d4eed
Matichai <notnoth43@gmail.com>
2022-02-26 15:10:37 +0700

    EDIT dOsURVEY

--- Dialogflow/index.js
+++ Dialogflow/index.js
@@ -864,32 +864,21 @@ const myexp = ((request, response) => {
             altText: "This is a buttons template",
             template: {
                 type: "buttons",
-                thumbnailImageUrl: "https://firebasestorage.googleapis.com/v0/b/nong-tung-jai-68673.appspot.com/o/%E0%B9%81%E0%B8%9A%E0%B8%9A%E0%B8%AA%E0%B8%AD%E0%B8%9A%E0%B8%96%E0%B8%B2%E0%B8%A1%E0%B8%84%E0%B8%A7%E0%B8%B2%E0%B8%A1%E0%B8%9E%E0%B8%B6%E0%B8%87%E0%B8%9E%E0%B8%AD%E0%B9%83%E0%B8%88%2FQR%E0%B9%81%E0%B8%9A%E0%B8%9A%E0%B8%AA%E0%B8%AD%E0%B8%9A%E0%B8%96%E0%B8%B2%E0%B8%A1%E0%B8%84%E0%B8%A7%E0%B8%B2%E0%B8%A1%E0%B8%9E%E0%B8%B6%E0%B8%87%E0%B8%9E%E0%B8%AD%E0%B9%83%E0%B8%88%E0%B8%A3%E0%B8%B0%E0%B8%9A%E0%B8%9A%E0%B9%81%E0%B8%8A%E0%B8%97%E0%B8%9A%E0%B8%AD%E0%B8%97%E0%B8%AA%E0%B8%B3%E0%B8%AB%E0%B8%A3%E0%B8%B1%E0%B8%9A%E0%B9%83%E0%B8%AB%E0%B9%89%E0%B8%84%E0%B8%B3%E0%B8%9B%E0%B8%A3%E0%B8%B6%E0%B8%81%E0%B8%A9%E0%B8%B2%E0%B9%81%E0%B8%A5%E0%B8%B0%E0%B8%95%E0%B8%B4%E0%B8%94%E0%B8%95%E0%B8%B2%E0%B8%A1%E0%B8%9B%E0%B8%B1%E0%B8%8D%E0%B8%AB%E0%B8%B2%E0%B8%81%E0%B8%B2%E0%B8%A3%E0%B8%94%E0%B8%B7%E0%B9%88%E0%B8%A1%E0%B9%81%E0%B8%AD%E0%B8%A5%E0%B8%81%E0%B8%AD%E0%B8%AE%E0%B8%AD%E0%B8%A5%E0%B9%8C.png?alt=media&token=5e3d75b1-6de6-41da-9822-fc697f6cec9f",
+                thumbnailImageUrl: "https://firebasestorage.googleapis.com/v0/b/nong-tung-jai-68673.appspot.com/o/%E0%B9%81%E0%B8%9A%E0%B8%9A%E0%B8%AA%E0%B8%AD%E0%B8%9A%E0%B8%96%E0%B8%B2%E0%B8%A1%E0%B8%84%E0%B8%A7%E0%B8%B2%E0%B8%A1%E0%B8%9E%E0%B8%B6%E0%B8%87%E0%B8%9E%E0%B8%AD%E0%B9%83%E0%B8%88%2Fonline-survey.jpg?alt=media&token=57f00317-4844-4475-809c-8c72ad3ca717",
                 imageAspectRatio: "rectangle",
                 imageSize: "cover",
                 imageBackgroundColor: "#FFFFFF",
-                title: "Menu",
-                text: "Please select",
+                title: "แบบประเมินความพึงพอใจ",
                 defaultAction: {
                     type: "uri",
-                    label: "View detail",
-                    uri: "http://example.com/page/123"
+                    label: "กดที่นี่เพื่อทำแบบสอบถาม",
+                    uri: "https://forms.gle/YJXZj2NyZeySvWbJA"
                 },
                 actions: [
                     {
-                      type: "postback",
-                      label: "Buy",
-                      data: "action=buy&itemid=123"
-                    },
-                    {
-                      type: "postback",
-                      label: "Add to cart",
-                      data: "action=add&itemid=123"
-                    },
-                    {
                       type: "uri",
-                      label: "View detail",
-                      uri: "http://example.com/page/123"
+                      label: "กดที่นี่เพื่อทำแบบสอบถาม",
+                      uri: "https://forms.gle/YJXZj2NyZeySvWbJA"
                     }
                 ]
             }
