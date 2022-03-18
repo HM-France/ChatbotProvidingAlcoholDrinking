3c4b555ad4e88622342866ccac47012c2a69cd5d
Matichai <notnoth43@gmail.com>
2022-03-18 21:16:15 +0700

    แก้ไข SetUSerPrepNext

--- Dialogflow/index.js
+++ Dialogflow/index.js
@@ -103,7 +103,7 @@ const myexp = ((request, response) => {
                         }
                     }
                 ]
-            },
+            }
         } , { sendAsMessage: true }));
     }
 
