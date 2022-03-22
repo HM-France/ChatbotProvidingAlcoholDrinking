9b186ebf76c9207a1af5043c809d101c7fce0974
Matichai <notnoth43@gmail.com>
2022-03-23 00:05:21 +0700

    .

--- Dialogflow/index.js
+++ Dialogflow/index.js
@@ -3,7 +3,7 @@ const { WebhookClient, Payload } = require('dialogflow-fulfillment');
 const { ADDRGETNETWORKPARAMS } = require('dns');
 const { userDB } = require('../firebase');
 const  imageCarousels = require('./imageCarousels');
-const knowladgeBase = require('./knowledgebase');
+const  knowladgeBase = require('./knowledgebase');
 
 const myexp = ((request, response) => {
     //Create an instance
@@ -1333,116 +1333,8 @@ const myexp = ((request, response) => {
     }
    
       const Test = async () => {
-            agent.add("กำลังทดสอบ") ; /*
-            agent.add(new Payload(`LINE` , {
-                type: "template",
-                altText: "This is a buttons template",
-                template: {
-                    type: "buttons",
-                    thumbnailImageUrl: "https://firebasestorage.googleapis.com/v0/b/nong-tung-jai-68673.appspot.com/o/%E0%B8%AA%E0%B8%B2%E0%B8%A3%E0%B8%B0%E0%B8%84%E0%B8%A7%E0%B8%B2%E0%B8%A1%E0%B8%A3%E0%B8%B9%E0%B9%89%2FDocument.jpg?alt=media&token=e9d7be53-561e-44c0-b7c2-4b8ae76164b0",              
-                    imageAspectRatio: "rectangle",
-                    imageSize: "cover",
-                    imageBackgroundColor: "#FFFFFF",
-                    titl: "Menu",
-                    text: "เชิญเลือกหัวข้อที่สนใจได้เลยค่ะ",
-                    defaultAction: {
-                        type: "message",
-                          label: "วิธีการใช้งาน",
-                          text: "วิธีการใช้งาน"
-                    },
-                    actions: [
-                        {
-                          type: "message",
-                          label: "เกี่ยวกับสุรา",
-                          text: "เกี่ยวกับสุรา"
-                        },
-                        {
-                            type: "message",
-                            label: "ผลกระทบจากสุรา",
-                            text: "ผลกระทบจากสุรา"
-                        },
-                        {
-                            type: "message",
-                            label: "เกี่ยวกับการเลิกสุรา",
-                            text: "เกี่ยวกับการเลิกสุรา"
-                        },
-                        {
-                            type: "message",
-                            label: "เบอร์ติดต่อฉุกเฉิน",
-                            text: "เบอร์ติดต่อฉุกเฉิน"
-                        }
-                    ]
-                }
-              } , { sendAsMessage:true})); */
-            agent.add(new Payload( `LINE`, {
-                
-                    type: "template",
-                    altText: "this is a carousel template",
-                    template: {
-                        type: "carousel",
-                        columns: [
-                            {
-                              thumbnailImageUrl: "https://example.com/bot/images/item1.jpg",
-                              imageBackgroundColor: "#FFFFFF",
-                              title: "this is menu",
-                              text: "description",
-                              defaultAction: {
-                                  type: "uri",
-                                  label: "View detail",
-                                  uri: "http://example.com/page/123"
-                              },
-                              "actions": [
-                                  {
-                                      type: "postback",
-                                      label: "Buy",
-                                      data: "action=buy&itemid=111"
-                                  },
-                                  {
-                                      type: "postback",
-                                      label: "Add to cart",
-                                      data: "action=add&itemid=111"
-                                  },
-                                  {
-                                      type: "uri",
-                                      label: "View detail",
-                                      uri: "http://example.com/page/111"
-                                  }
-                              ]
-                            },
-                            {
-                              thumbnailImageUrl: "https://example.com/bot/images/item2.jpg",
-                              imageBackgroundColor: "#000000",
-                              title: "this is menu",
-                              text: "description",
-                              defaultAction: {
-                                  type: "uri",
-                                  label: "View detail",
-                                  uri: "http://example.com/page/222"
-                              },
-                              actions: [
-                                  {
-                                      type: "postback",
-                                      label: "Buy",
-                                      data: "action=buy&itemid=222"
-                                  },
-                                  {
-                                      type: "postback",
-                                      label: "Add to cart",
-                                      data: "action=add&itemid=222"
-                                  },
-                                  {
-                                      type: "uri",
-                                      label: "View detail",
-                                      uri: "http://example.com/page/222"
-                                  }
-                              ]
-                            }
-                        ],
-                        imageAspectRatio: "rectangle",
-                        imageSize: "cover"
-                    }
-                  
-            } , { sendAsMessage:true} ));
+            agent.add("กำลังทดสอบ") ; 
+            return agent.add(new Payload( `LINE`, knowladgeBase.Flex() , { sendAsMessage:true} ));
             agent.add("สิ้นสุดการทดสอบ");
       }
 
--- Dialogflow/knowledgebase.js
+++ Dialogflow/knowledgebase.js
@@ -1,47 +1,112 @@
-const general = () => {
+const MainMenu = () => {
     return({
         type: "template",
         altText: "This is a buttons template",
         template: {
             type: "buttons",
-            thumbnailImageUrl: "https://example.com/bot/images/image.jpg",
+            thumbnailImageUrl: "https://firebasestorage.googleapis.com/v0/b/nong-tung-jai-68673.appspot.com/o/%E0%B8%AA%E0%B8%B2%E0%B8%A3%E0%B8%B0%E0%B8%84%E0%B8%A7%E0%B8%B2%E0%B8%A1%E0%B8%A3%E0%B8%B9%E0%B9%89%2FDocument.jpg?alt=media&token=e9d7be53-561e-44c0-b7c2-4b8ae76164b0",              
             imageAspectRatio: "rectangle",
             imageSize: "cover",
             imageBackgroundColor: "#FFFFFF",
             title: "Menu",
-            text: "Please select",
+            text: "เชิญเลือกหัวข้อที่สนใจได้เลยค่ะ",
             defaultAction: {
-                type: "uri",
-                label: "View detail",
-                uri: "http://example.com/page/123"
+                type: "message",
+                  label: "วิธีการใช้งาน",
+                  text: "วิธีการใช้งาน"
             },
             actions: [
                 {
                   type: "message",
-                  label: "สุราคืออะไร",
-                  text: "สุราคืออะไร"                
+                  label: "เกี่ยวกับสุรา",
+                  text: "เกี่ยวกับสุรา"
                 },
                 {
                     type: "message",
-                    label: "ร่างกายเผาผลาญแอลกอฮอล์อย่างไร",
-                    text: "ร่างกายเผาผลาญแอลกอฮอล์อย่างไร"                
+                    label: "ผลกระทบจากสุรา",
+                    text: "ผลกระทบจากสุรา"
                 },
                 {
                     type: "message",
-                    label: "อาการคนเมาสุรา",
-                    text: "อาการคนเมาสุรา"                
+                    label: "เกี่ยวกับการเลิกสุรา",
+                    text: "เกี่ยวกับการเลิกสุรา"
                 },
                 {
                     type: "message",
-                    label: "อาการคนติดสุรา",
-                    text: "อาการคนติดสุรา"                
-                },
-                {
-                    type: "message",
-                    label: "อาการถอนพิษสุรา",
-                    text: "อาการถอนพิษสุรา"                
+                    label: "เบอร์ติดต่อฉุกเฉิน",
+                    text: "เบอร์ติดต่อฉุกเฉิน"
                 }
             ]
         }
       })
+}
+
+const Flex = () => {
+    return ({
+        type: "template",
+        altText: "this is a carousel template",
+        template: {
+            type: "carousel",
+            columns: [
+                {
+                  thumbnailImageUrl: "https://example.com/bot/images/item1.jpg",
+                  imageBackgroundColor: "#FFFFFF",
+                  title: "this is menu",
+                  text: "description",
+                  defaultAction: {
+                      type: "uri",
+                      label: "View detail",
+                      uri: "http://example.com/page/123"
+                  },
+                  "actions": [
+                      {
+                          type: "postback",
+                          label: "Buy",
+                          data: "action=buy&itemid=111"
+                      },
+                      {
+                          type: "postback",
+                          label: "Add to cart",
+                          data: "action=add&itemid=111"
+                      },
+                      {
+                          type: "uri",
+                          label: "View detail",
+                          uri: "http://example.com/page/111"
+                      }
+                  ]
+                },
+                {
+                  thumbnailImageUrl: "https://example.com/bot/images/item2.jpg",
+                  imageBackgroundColor: "#000000",
+                  title: "this is menu",
+                  text: "description",
+                  defaultAction: {
+                      type: "uri",
+                      label: "View detail",
+                      uri: "http://example.com/page/222"
+                  },
+                  actions: [
+                      {
+                          type: "postback",
+                          label: "Buy",
+                          data: "action=buy&itemid=222"
+                      },
+                      {
+                          type: "postback",
+                          label: "Add to cart",
+                          data: "action=add&itemid=222"
+                      },
+                      {
+                          type: "uri",
+                          label: "View detail",
+                          uri: "http://example.com/page/222"
+                      }
+                  ]
+                }
+            ],
+            imageAspectRatio: "rectangle",
+            imageSize: "cover"
+        }
+    })
 }
