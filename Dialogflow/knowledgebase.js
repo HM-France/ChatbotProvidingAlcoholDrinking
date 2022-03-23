94b10f5ea4043c5540db3ec83aea070347bb5a4b
Matichai <notnoth43@gmail.com>
2022-03-23 23:43:17 +0700

    เพิ่มความรู้หมวดการเลิก

--- Dialogflow/knowledgebase.js
+++ Dialogflow/knowledgebase.js
@@ -162,6 +162,86 @@ const AlcoholIllness = () => {
       })
 }
 
+
+const AlcoholTreatment = () => {
+    return ({
+        type: "template",
+        altText: "เกี่ยวกับการเลิกสุรา",
+        template: {
+            type: "carousel",
+            columns: [
+                {
+                  thumbnailImageUrl: "https://firebasestorage.googleapis.com/v0/b/nong-tung-jai-68673.appspot.com/o/%E0%B8%AA%E0%B8%B2%E0%B8%A3%E0%B8%B0%E0%B8%84%E0%B8%A7%E0%B8%B2%E0%B8%A1%E0%B8%A3%E0%B8%B9%E0%B9%89%2FDocument.jpg?alt=media&token=e9d7be53-561e-44c0-b7c2-4b8ae76164b0",
+                  imageBackgroundColor: "#FFFFFF",
+                  title: "วิธีการเลิกสุรา",
+                  text: "เชิญเลือกหัวข้อที่สนใจได้เลยค่ะ",
+                  defaultAction: {
+                    label: "วิธีการใช้งาน",
+                    text: "วิธีการใช้งาน"
+                  },
+                  actions: [
+                    {
+                        type: "message",
+                        label: "การเลิกสุราด้วยตัวเอง",
+                        text: "การเลิกสุราด้วยตัวเอง"
+                      },
+                      {
+                          type: "message",
+                          label: "การเลิกสุรากับหมอ",
+                          text: "การเลิกสุรากับหมอ"
+                      },
+                      {
+                          type: "message",
+                          label: "การป้องกันการกลับไปดื่มซ้ำ",
+                          text: "การป้องกันการกลับไปดื่มซ้ำ"
+                      },
+                      {
+                          type: "message",
+                          label: "น้ำดื่มทางเลือก",
+                          text: "น้ำดื่มทางเลือก"
+                      }
+                  ]
+                },
+                {
+                  thumbnailImageUrl: "https://firebasestorage.googleapis.com/v0/b/nong-tung-jai-68673.appspot.com/o/%E0%B8%AA%E0%B8%B2%E0%B8%A3%E0%B8%B0%E0%B8%84%E0%B8%A7%E0%B8%B2%E0%B8%A1%E0%B8%A3%E0%B8%B9%E0%B9%89%2FDocument.jpg?alt=media&token=e9d7be53-561e-44c0-b7c2-4b8ae76164b0",
+                  imageBackgroundColor: "#000000",
+                  title: "วิธีช่วยคนให้เลิกสุรา",
+                  text: "เชิญเลือกหัวข้อที่สนใจได้เลยค่ะ",
+                  defaultAction: {
+                    label: "วิธีการใช้งาน",
+                    text: "วิธีการใช้งาน"
+                  },
+                  actions: [
+                    {
+                        type: "message",
+                        label: "การช่วยผู้ติดสุรา",
+                        text: "การช่วยผู้ติดสุรา"
+                      },
+                      {
+                          type: "message",
+                          label: "การแก้อาการเมาค้าง",
+                          text: "การแก้อาการเมาค้าง"
+                      },
+                      {
+                          type: "message",
+                          label: "เมื่อเสี่ยงทำร้ายตัวเอง",
+                          text: "เมื่อเสี่ยงทำร้ายตัวเอง"
+                      },
+                      {
+                          type: "message",
+                          label: "ใครที่ควรพบแพทย์",
+                          text: "ใครที่ควรพบแพทย์"
+                      }
+                  ]
+                }
+            ],
+            imageAspectRatio: "rectangle",
+            imageSize: "cover"
+        }
+    })
+}
+
+
 const Flex = () => {
     return ({
         type: "template",
@@ -232,4 +312,4 @@ const Flex = () => {
     })
 }
 
-module.exports = {Flex,MainMenu,IncludingAlcohol,AlcoholEffect,AlcoholIllness}
+module.exports = {Flex,MainMenu,IncludingAlcohol,AlcoholEffect,AlcoholIllness,AlcoholTreatment}
