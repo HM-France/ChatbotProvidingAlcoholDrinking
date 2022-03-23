f4fe48dc38a1baca7b36ddaa2d1d237fbd77e35e
Teerapat28 <poo28kung@gmail.com>
2022-03-23 18:01:00 +0700

    ยกเลิกการทำ delay

--- Dialogflow/index.js
+++ Dialogflow/index.js
@@ -5,10 +5,6 @@ const { userDB } = require('../firebase');
 const imageCarousels = require('./imageCarousels');
 const knowladgeBase = require('./knowledgebase');
 
-function delay(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
-
 const myexp = ((request, response) => {
     //Create an instance
     const agent = new WebhookClient({ request, response });
@@ -1057,11 +1053,10 @@ const myexp = ((request, response) => {
         if (!numberofDrinks) {
             return agent.add(`ดื่มไปปริมาณกี่${container}คะ?`);
         }
+
         agent.add(`น้องตั้งใจขอทบทวนข้อมูลนะคะ`);
-        await new Promise(resolve => setTimeout(resolve, 1000));
         agent.add(`จากข้อมูลที่น้องตั้งใจได้รับมาคือ คุณเป็น ผู้${gender} น้ำหนัก ${weight} กิโลกรัม ดื่ม${types}ไปทั้งหมด ${numberofDrinks} ${container} โดยหนึ่ง${container}มีปริมาณ ${volume} มิลลิลิตร  `);
         console.log('Redirecting to : measureAlcohalInBloodCalculated');
-        
         return agent.add(new Payload('LINE', {
             "type": "text",
             "text": "ข้อมูลนี้ถูกต้องมั๊ยคะ",
