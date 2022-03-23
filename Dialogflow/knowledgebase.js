9721c2d97b4cca124dc3b899ac6269c68c9b045c
Matichai <notnoth43@gmail.com>
2022-03-24 00:20:49 +0700

    .

--- Dialogflow/knowledgebase.js
+++ Dialogflow/knowledgebase.js
@@ -171,41 +171,48 @@ const AlcoholTreatment = () => {
             type: "carousel",
             columns: [
                 {
-                    thumbnailImageUrl: "https://firebasestorage.googleapis.com/v0/b/nong-tung-jai-68673.appspot.com/o/%E0%B8%AA%E0%B8%B2%E0%B8%A3%E0%B8%B0%E0%B8%84%E0%B8%A7%E0%B8%B2%E0%B8%A1%E0%B8%A3%E0%B8%B9%E0%B9%89%2FDocument.jpg?alt=media&token=e9d7be53-561e-44c0-b7c2-4b8ae76164b0",
-                    imageBackgroundColor: "#FFFFFF",
-                    title: "วิธีการเลิกสุรา",
-                    text: "เชิญเลือกหัวข้อที่สนใจได้เลยค่ะ",
-                    defaultAction: {
-                      label: "วิธีการใช้งาน",
-                      text: "วิธีการใช้งาน"
-                    },
-                  "actions": [
-                      {
-                          type: "postback",
-                          label: "Buy",
-                          data: "action=buy&itemid=111"
+                  thumbnailImageUrl: "https://example.com/bot/images/item1.jpg",
+                  imageBackgroundColor: "#FFFFFF",
+                  title: "this is menu",
+                  text: "description",
+                  defaultAction: {
+                      type: "uri",
+                      label: "View detail",
+                      uri: "http://example.com/page/123"
+                  },
+                  actions: [
+                     {
+                        type: "message",
+                        label: "การเลิกด้วยตัวเอง",
+                        text: "การเลิกด้วยตัวเอง"
                       },
                       {
-                          type: "postback",
-                          label: "Add to cart",
-                          data: "action=add&itemid=111"
+                          type: "message",
+                          label: "การเลิกกับหมอ",
+                          text: "การเลิกกับหมอ"
                       },
                       {
-                          type: "uri",
-                          label: "View detail",
-                          uri: "http://example.com/page/111"
+                          type: "message",
+                          label: "การป้องกันการดื่มซ้ำ",
+                          text: "การป้องกันการดื่มซ้ำ"
+                      },
+                      {
+                          type: "message",
+                          label: "น้ำดื่มทางเลือก",
+                          text: "น้ำดื่มทางเลือก"
                       }
                   ]
                 },
                 {
-                    thumbnailImageUrl: "https://firebasestorage.googleapis.com/v0/b/nong-tung-jai-68673.appspot.com/o/%E0%B8%AA%E0%B8%B2%E0%B8%A3%E0%B8%B0%E0%B8%84%E0%B8%A7%E0%B8%B2%E0%B8%A1%E0%B8%A3%E0%B8%B9%E0%B9%89%2FDocument.jpg?alt=media&token=e9d7be53-561e-44c0-b7c2-4b8ae76164b0",
-                    imageBackgroundColor: "#FFFFFF",
-                    title: "วิธีการเลิกสุรา",
-                    text: "เชิญเลือกหัวข้อที่สนใจได้เลยค่ะ",
-                    defaultAction: {
-                      label: "วิธีการใช้งาน",
-                      text: "วิธีการใช้งาน"
-                    },
+                  thumbnailImageUrl: "https://example.com/bot/images/item2.jpg",
+                  imageBackgroundColor: "#000000",
+                  title: "this is menu",
+                  text: "description",
+                  defaultAction: {
+                      type: "uri",
+                      label: "View detail",
+                      uri: "http://example.com/page/222"
+                  },
                   actions: [
                       {
                           type: "postback",
