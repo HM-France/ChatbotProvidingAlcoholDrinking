e5d94930a52588502e3931d6aeb39663b56df8ed
Matichai <notnoth43@gmail.com>
2022-03-26 00:09:37 +0700

    edit fn knowladge_etc

--- Dialogflow/index.js
+++ Dialogflow/index.js
@@ -1422,7 +1422,12 @@ const myexp = ((request, response) => {
             }
 
             const knowladge_etc = async () => {
-                agent.add("Infographic knowladge_etc") ;
+                agent.add("นอกจากสุราจะก่อให้เกิดผลกระทบทางด้านสุขภาพมากมายแล้ว สุรายังทำให้เกิดลกระทบต่าง ๆ นอกจากสุขภาพด้วยเช่นกัน") ;
+                return agent.add(new Payload(`LINE` , {
+                    "type": "image",
+                    "originalContentUrl": "https://firebasestorage.googleapis.com/v0/b/nong-tung-jai-68673.appspot.com/o/%E0%B8%AA%E0%B8%B2%E0%B8%A3%E0%B8%B0%E0%B8%84%E0%B8%A7%E0%B8%B2%E0%B8%A1%E0%B8%A3%E0%B8%B9%E0%B9%89%2F%E0%B8%9C%E0%B8%A5%E0%B8%81%E0%B8%A3%E0%B8%B0%E0%B8%97%E0%B8%9A%E0%B8%AD%E0%B8%B7%E0%B9%88%E0%B8%99%E0%B9%86.png?alt=media&token=8d9e5071-5d4c-44cd-b9bf-40744f208121",
+                    "previewImageUrl": "https://firebasestorage.googleapis.com/v0/b/nong-tung-jai-68673.appspot.com/o/%E0%B8%AA%E0%B8%B2%E0%B8%A3%E0%B8%B0%E0%B8%84%E0%B8%A7%E0%B8%B2%E0%B8%A1%E0%B8%A3%E0%B8%B9%E0%B9%89%2F%E0%B8%9C%E0%B8%A5%E0%B8%81%E0%B8%A3%E0%B8%B0%E0%B8%97%E0%B8%9A%E0%B8%AD%E0%B8%B7%E0%B9%88%E0%B8%99%E0%B9%86.png?alt=media&token=8d9e5071-5d4c-44cd-b9bf-40744f208121"
+                  } , { sendAsMessage:true }));
             }
 
         const knowladge_Treatment = async () => {
