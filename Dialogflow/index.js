5bff590e85564cc798a78761198a42efff8e5b31
Matichai <notnoth43@gmail.com>
2022-03-25 18:06:31 +0700

    edit fn knowladge_AlcoholAddiction

--- Dialogflow/index.js
+++ Dialogflow/index.js
@@ -1320,7 +1320,7 @@ const myexp = ((request, response) => {
             }
 
             const knowladge_Drunkenness = async () => {
-                agent.add("การเมาสุราจะแสดงออกชัดเจนมากเพียงใด ขึ้นอยู่กับว่าในร่างกายนั้นมีปริมาณแอลกอฮอล์อยู่มากน้อยเพียงใด ซึ่งแบ่งได้ตามนี้ค่ะ") ;
+                agent.add("อาการเมาสุราจะแสดงออกชัดเจนมากเพียงใด ขึ้นอยู่กับว่าในร่างกายนั้นมีปริมาณแอลกอฮอล์อยู่มากน้อยเพียงใด ซึ่งแบ่งได้ตามนี้ค่ะ ⬇️") ;
                 return agent.add(new Payload(`LINE` , {
                     "type": "image",
                     "originalContentUrl": "https://firebasestorage.googleapis.com/v0/b/nong-tung-jai-68673.appspot.com/o/%E0%B8%AA%E0%B8%B2%E0%B8%A3%E0%B8%B0%E0%B8%84%E0%B8%A7%E0%B8%B2%E0%B8%A1%E0%B8%A3%E0%B8%B9%E0%B9%89%2F%E0%B8%AD%E0%B8%B2%E0%B8%81%E0%B8%B2%E0%B8%A3%E0%B8%82%E0%B8%AD%E0%B8%87%E0%B8%84%E0%B8%99%E0%B9%80%E0%B8%A1%E0%B8%B2%E0%B8%AA%E0%B8%B8%E0%B8%A3%E0%B8%B2.png?alt=media&token=2bcaef0b-a3af-4972-9919-44c759befff1",
@@ -1329,7 +1329,12 @@ const myexp = ((request, response) => {
             }
 
             const knowladge_AlcoholAddiction = async () => {
-                agent.add("Infographic");
+                agent.add("ผู้ที่ติดสุราจะมีพฤติกรรมบ่งชี้โดยคร่าว 7 ข้อ ซึ่งหากมีพฤติกรรมที่เข้าข่ายอย่างน้อย 3 ใน 7 ข้อ ก็อาจสรุปได้ว่าเป็นผู้ติดสุราค่ะ");
+                agent.add(new Payload(`LINE` , {
+                    "originalContentUrl": "https://firebasestorage.googleapis.com/v0/b/nong-tung-jai-68673.appspot.com/o/%E0%B8%AA%E0%B8%B2%E0%B8%A3%E0%B8%B0%E0%B8%84%E0%B8%A7%E0%B8%B2%E0%B8%A1%E0%B8%A3%E0%B8%B9%E0%B9%89%2F7%20%E0%B8%A5%E0%B8%B1%E0%B8%81%E0%B8%A9%E0%B8%93%E0%B8%B0%E0%B8%82%E0%B8%AD%E0%B8%87%E0%B8%84%E0%B8%99%E0%B8%95%E0%B8%B4%E0%B8%94%E0%B8%AA%E0%B8%B8%E0%B8%A3%E0%B8%B2.png?alt=media&token=a31e4a8c-8b87-48c9-a827-a66eb65d0fbd",
+                    "previewImageUrl": "https://firebasestorage.googleapis.com/v0/b/nong-tung-jai-68673.appspot.com/o/%E0%B8%AA%E0%B8%B2%E0%B8%A3%E0%B8%B0%E0%B8%84%E0%B8%A7%E0%B8%B2%E0%B8%A1%E0%B8%A3%E0%B8%B9%E0%B9%89%2F7%20%E0%B8%A5%E0%B8%B1%E0%B8%81%E0%B8%A9%E0%B8%93%E0%B8%B0%E0%B8%82%E0%B8%AD%E0%B8%87%E0%B8%84%E0%B8%99%E0%B8%95%E0%B8%B4%E0%B8%94%E0%B8%AA%E0%B8%B8%E0%B8%A3%E0%B8%B2.png?alt=media&token=a31e4a8c-8b87-48c9-a827-a66eb65d0fbd"
+                  } , { sendAsMessage:true }));
+                agent.add("แต่อย่างไรก็ตามการที่จะยืนยันอย่างเป็นทางการได้ว่าใครเป็นผู้ติดสุราหรือไม่จะขึ้นอยู่กับดุลยพินิจของแพทย์นะคะ") ;
             }
 
             const knowladge_WithdrawalkSymptoms = async () => {
