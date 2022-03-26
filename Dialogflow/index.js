08e48e62b13511765b93ddc68ae59097c4d21c2d
Matichai <notnoth43@gmail.com>
2022-03-26 23:27:26 +0700

    add fn knowladge_Housing

--- Dialogflow/index.js
+++ Dialogflow/index.js
@@ -1508,6 +1508,10 @@ const myexp = ((request, response) => {
                 return agent.add(new Payload(`LINE` , knowladgeBase.HelpCenter() , { sendAsMessage:true } ));
             }
 
+            const knowladge_Housing = async () => {
+
+            }
+            
     const knowladge_HowToUse = async () => {
         agent.add("วิธีการใช้งานน้องตั้งใจในส่วนของข้อมูลเลิกเหล้าค่ะ") ;
     }
@@ -1519,219 +1523,6 @@ const myexp = ((request, response) => {
 
     const Test = async () => {
         agent.add("กำลังทดสอบ");
-        return agent.add(new Payload(`LINE` ,  {
-            "type": "flex",
-            "altText": "Flex Message",
-            "contents": {
-              "type": "bubble",
-              "body": {
-                "type": "box",
-                "layout": "vertical",
-                "contents": [
-                  {
-                    "type": "text",
-                    "text": "RECEIPT",
-                    "weight": "bold",
-                    "color": "#1DB446",
-                    "size": "sm"
-                  },
-                  {
-                    "type": "text",
-                    "text": "Brown Store",
-                    "weight": "bold",
-                    "size": "xxl",
-                    "margin": "md"
-                  },
-                  {
-                    "type": "text",
-                    "text": "Miraina Tower, 4-1-6 Shinjuku, Tokyo",
-                    "size": "xs",
-                    "color": "#aaaaaa",
-                    "wrap": true
-                  },
-                  {
-                    "type": "separator",
-                    "margin": "xxl"
-                  },
-                  {
-                    "type": "box",
-                    "layout": "vertical",
-                    "margin": "xxl",
-                    "spacing": "sm",
-                    "contents": [
-                      {
-                        "type": "box",
-                        "layout": "horizontal",
-                        "contents": [
-                          {
-                            "type": "text",
-                            "text": "Energy Drink",
-                            "size": "sm",
-                            "color": "#555555"
-                          },
-                          {
-                            "type": "text",
-                            "text": "$2.99",
-                            "size": "sm",
-                            "color": "#111111",
-                            "align": "end"
-                          }
-                        ]
-                      },
-                      {
-                        "type": "box",
-                        "layout": "horizontal",
-                        "contents": [
-                          {
-                            "type": "text",
-                            "text": "Chewing Gum",
-                            "size": "sm",
-                            "color": "#555555"
-                          },
-                          {
-                            "type": "text",
-                            "text": "$0.99",
-                            "size": "sm",
-                            "color": "#111111",
-                            "align": "end"
-                          }
-                        ]
-                      },
-                      {
-                        "type": "box",
-                        "layout": "horizontal",
-                        "contents": [
-                          {
-                            "type": "text",
-                            "text": "Bottled Water",
-                            "size": "sm",
-                            "color": "#555555"
-                          },
-                          {
-                            "type": "text",
-                            "text": "$3.33",
-                            "size": "sm",
-                            "color": "#111111",
-                            "align": "end"
-                          }
-                        ]
-                      },
-                      {
-                        "type": "separator",
-                        "margin": "xxl"
-                      },
-                      {
-                        "type": "box",
-                        "layout": "horizontal",
-                        "margin": "xxl",
-                        "contents": [
-                          {
-                            "type": "text",
-                            "text": "ITEMS",
-                            "size": "sm",
-                            "color": "#555555"
-                          },
-                          {
-                            "type": "text",
-                            "text": "3",
-                            "size": "sm",
-                            "color": "#111111",
-                            "align": "end"
-                          }
-                        ]
-                      },
-                      {
-                        "type": "box",
-                        "layout": "horizontal",
-                        "contents": [
-                          {
-                            "type": "text",
-                            "text": "TOTAL",
-                            "size": "sm",
-                            "color": "#555555"
-                          },
-                          {
-                            "type": "text",
-                            "text": "$7.31",
-                            "size": "sm",
-                            "color": "#111111",
-                            "align": "end"
-                          }
-                        ]
-                      },
-                      {
-                        "type": "box",
-                        "layout": "horizontal",
-                        "contents": [
-                          {
-                            "type": "text",
-                            "text": "CASH",
-                            "size": "sm",
-                            "color": "#555555"
-                          },
-                          {
-                            "type": "text",
-                            "text": "$8.0",
-                            "size": "sm",
-                            "color": "#111111",
-                            "align": "end"
-                          }
-                        ]
-                      },
-                      {
-                        "type": "box",
-                        "layout": "horizontal",
-                        "contents": [
-                          {
-                            "type": "text",
-                            "text": "CHANGE",
-                            "size": "sm",
-                            "color": "#555555"
-                          },
-                          {
-                            "type": "text",
-                            "text": "$0.69",
-                            "size": "sm",
-                            "color": "#111111",
-                            "align": "end"
-                          }
-                        ]
-                      }
-                    ]
-                  },
-                  {
-                    "type": "separator",
-                    "margin": "xxl"
-                  },
-                  {
-                    "type": "box",
-                    "layout": "horizontal",
-                    "margin": "md",
-                    "contents": [
-                      {
-                        "type": "text",
-                        "text": "PAYMENT ID",
-                        "size": "xs",
-                        "color": "#aaaaaa"
-                      },
-                      {
-                        "type": "text",
-                        "text": "#743289384279",
-                        "color": "#aaaaaa",
-                        "size": "xs",
-                        "align": "end"
-                      }
-                    ]
-                  }
-                ]
-              },
-              "styles": {
-                "footer": {
-                  "separator": true
-                }
-              }
-            }
-          } , { sendAsMessage:true }));
         agents.add("สิ้นสุดการทดสอบ");
     }
 
@@ -1795,6 +1586,7 @@ const myexp = ((request, response) => {
             intentMap.set('select_HowToUseTreatment', knowladge_HowToUse);
         intentMap.set('select_Contact', knowladge_Contact);
             intentMap.set('select_HelpingCenter', knowladge_HelpingCenter);
+            intentMap.set('select_Housing', knowladge_Housing);
     agent.handleRequest(intentMap);
 });
 module.exports = myexp
